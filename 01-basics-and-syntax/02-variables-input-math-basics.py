d41c38ac93d0137e92d5441bf7359221d8839b62
Eyad-Elghonemy <eyad0758@gmail.com>
2026-08-26 18:03:24 +0300

    Add 02-variables-input-math-basics.py

---
+++ 01-basics-and-syntax/02-variables-input-math-basics.py
@@ -0,0 +1,58 @@
+# name = "Ola"
+# print (f"name : {name}")
+# num = 3
+# num = 7
+# # Overwrite
+# print(num)
+
+# fracton_num =  7.3
+# print(fracton_num)
+# is_boolean = False
+# print(is_boolean)
+
+# name = input(("Enter Your Name : "))
+# print(f"Hello {name}")
+
+# orange_input = int(input("How Many Oranges Did You Take : "))
+# num_oranges = 5 - orange_input
+# print(f"Number Of Oranges Currently Is : {str(num_oranges)}")
+
+# name = "My Name Is 'Ola'"
+# last_name = 'Ahmed isn\'t'
+# message = '''My Name Is Ola 
+# Welacome To Python Program'''
+# print(last_name)
+# print(message)
+
+# first_name = "Ola"
+# last_name = "Ahmed"
+# message = "My First Name Is : " + first_name + " And My Last Name Is " + last_name
+# msg = f"My First Name Is {first_name} And My Last Name Is {last_name}"
+
+# print(message)
+# print(msg)
+
+# name = "Python Program"
+# print(name.replace("thon", "air"))
+
+# num = 10
+# num2 =  3
+
+# num += 10  # num = num + 10
+# print(num ** num2)
+
+# brackets ()
+# expon power(**)
+# mult or division * /
+# add subt + -
+
+# res = (5 + 3) * 2 ** 3
+# print(res)
+
+from math import *  
+
+print(round(2.4))
+print(abs(-5))
+print(floor(3.9))
+print(ceil(6.1))
+
