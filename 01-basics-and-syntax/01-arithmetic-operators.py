88f0bc987fb90fed858cb01ac3bc5d0a6d73056f
Eyad-Elghonemy <eyad0758@gmail.com>
2026-08-26 18:02:58 +0300

    Upload /01-basics-and-syntax

---
+++ 01-basics-and-syntax/01-arithmetic-operators.py
@@ -0,0 +1,56 @@
+# -------------------------------
+# -- arthemtic operators -- 
+# -------------------------------
+
+# [+] addition
+# [-] subtraction
+# [*] multiplication
+# [/] division
+# [%] modulus
+# [**] exponent
+# [//] floor division
+# -------------------------------
+
+# addition 
+
+print(10+30) #40
+print(-10+20) # 10
+print(1+2.66) #3.66
+print(1.2+1.2) #2.4
+
+print(60-30) #30
+print(-30 - 20) #-50
+print(-30- -20) #-10
+print(5.66-3.44) #2.22
+
+# multiplication 
+
+print(10 *3) #30 
+print(5+10*100) #1005
+print((5+10)*100) #1500
+
+# division
+
+print(100/20) #5.0
+print(int(100/20)) #5
+
+# modulus
+
+print(8 %2) #0
+print(9%2) #1
+print(22%5) #2
+
+# exponent
+print(2**5) #32
+print(2*2*2*2*2) #32
+print(5**4) #625
+print(5*5*5*5) #625
+
+# floor division
+
+print(100//20) #5
+print(119//20) #5
+print(120//20) #6
+print(122//20) #6
+print(140//20) #7 
+print(143//20) #7
