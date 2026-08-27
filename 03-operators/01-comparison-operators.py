32b405e1ce18224b6059e63bafd7d31086c53fab
Eyad-Elghonemy <eyad0758@gmail.com>
2026-08-27 16:35:15 +0300

    Add /03-operators

---
+++ 03-operators/01-comparison-operators.py
@@ -0,0 +1,56 @@
+# --------------------------
+# -- Comparison Operators --
+# --------------------------
+# [ == ] Equal
+# [ != ] Not Equal
+# [ > ] Greater Than
+# [ < ] Less Than
+# [ >= ] Greater Than or Equal
+# [ <= ] Less Than or Equal
+# --------------------------
+
+# Equal + Not Equal
+
+print(100 == 100)
+print(100==200)
+print(100==100.00)
+
+print("="*50)
+
+print(100 != 100)
+print(100 != 200)
+print(100 != 100.00)
+
+# Greater Than + Less Than
+
+print("="*50)
+
+print(100 > 100)
+print(100 > 200)
+print(100 > 100.00)
+print(100 > 40)
+
+print("="*50)
+
+print(100 < 100)
+print(100 < 200)
+print(100 < 100.00)
+print(100 < 40)
+
+print("="*50)
+
+# Greater Than or Equal + Less Than or Equal
+
+print("="*50)
+
+print(100 >= 100)
+print(100 >= 200)
+print(100 >= 100.00)
+print(100 >= 40)
+
+print("="*50)
+
+print(100 <= 100)
+print(100 <= 200)
+print(100 <= 100.00)
+print(100 <= 40)
