054c63e418ef4d6418d2c63ddc4c689e6654e089
Eyad-Elghonemy <eyad0758@gmail.com>
2026-08-27 16:36:38 +0300

    Add 05-assignment-operators.py

---
+++ 03-operators/05-assignment-operators.py
@@ -0,0 +1,23 @@
+# -------------------------------
+# -- Assignment Operators --
+# -------------------------------
+# =
+# +=
+# -=
+# /=
+# **=
+# %=
+# //=
+# -------------------------------
+
+x = 10 # var one
+y = 20 # var two
+
+# var one = self [operator ] var two
+# var one [operator ] = var two
+
+# x += y
+
+x-= y # x= x-y 
+
+print(x)
