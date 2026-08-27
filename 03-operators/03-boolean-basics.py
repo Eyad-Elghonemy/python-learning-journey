00e03afc6deef939fe1f1f3de577831980639793
Eyad-Elghonemy <eyad0758@gmail.com>
2026-08-27 16:35:59 +0300

    Add 03-boolean-basics.py

---
+++ 03-operators/03-boolean-basics.py
@@ -0,0 +1,40 @@
+# -------------------------------
+# -- Boolean -- 
+# -------------
+# [1] in programming you need to know if your code output is true or false 
+# [2] boolean values are the two constant objects False + True
+#  -------------------------------
+
+name = ""
+
+print(name.isspace())
+
+print("="*50)
+
+print(100>200)
+print(100>90)
+
+print("="*50)
+
+# True values
+
+print(bool("osama"))
+print(bool(100))
+print(bool(100.95))
+print(bool([1,2,3,4,5]))
+
+print("="*50)
+
+# False values
+
+print(bool(0))
+print(bool(""))
+print(bool(''))
+print(bool([]))
+print(bool(()))
+print(bool(False))
+print(bool({}))
+print(bool(None))
+
+print("="*50)
+
