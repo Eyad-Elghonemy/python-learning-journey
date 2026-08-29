5bdab1ce53ab5afd832543d798d307cc23f2449f
Eyad-Elghonemy <eyad0758@gmail.com>
2026-08-30 01:58:36 +0300

    Add files via upload

---
+++ 06-loops/01-while-loop-basics.py
@@ -0,0 +1,21 @@
+# -------------------
+# -- Loop => While --
+# -------------------
+# While Condition_is_true 
+#   Code Will Run
+
+a = 0
+
+while a < 15 :
+    
+    print(a)
+    
+    a += 1  # a = a + 1
+    
+else :
+    
+    print ("Loop Is Done") # True Become False 
+    
+# while false :
+
+    #print("will not print")
