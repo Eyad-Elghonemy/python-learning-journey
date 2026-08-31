daf12407c85e52eba7540517b5a6e45fd76adea5
Eyad-Elghonemy <eyad0758@gmail.com>
2026-08-31 03:39:39 +0300

    Add /07-functions

---
+++ 07-functions/01-function-basics-and-return.py
@@ -0,0 +1,25 @@
+# -------------------------
+# -- Function And Return --
+# -------------------------
+# [1] A Function Is A Reusable Block Of Code Do A Task
+# [2] A Function Run When You Call It
+# [3] A Function Accept Element To Deal With Called [Parameters]
+# [4] A Function Can Do The Task Without Returning Data 
+# [5] A Function Can Return Data After Jop Is Finished
+# [6] A Function Create To Prevent DRY [Dpn't Repeat Yourself]
+# [7] A Function Accept Elements when You Call It Called [Arguements]
+# [8] There's A Built=in Functions And User Defined Functions
+# [9] A Function Is For All Team And All Apps
+# ---------------------------------------
+
+def function_name() :
+    
+    return "Hello Python From Inside Function"
+    
+#    print("Hello Python From Inside Function")
+    
+#function_name()
+
+datafromfunction =  function_name()
+
+print(datafromfunction)
