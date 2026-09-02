d6cd342e8ce6ef0f8e1f5d1ee4db4386fdd04a42
Eyad-Elghonemy <eyad0758@gmail.com>
2026-09-02 08:21:31 +0300

    Add 11-pylint-and-clean-code.py

---
+++ 07-functions/11-pylint-and-clean-code.py
@@ -0,0 +1,19 @@
+# -----------------------------------------------
+# -- Installing And Use Pylint For Better Code --
+# -----------------------------------------------
+
+
+"""
+This Is My Module 
+To Create Function
+To Say Hello
+"""
+
+def say_hello(name) :
+    """This Function Only say Hello To Someone"""
+    msg = "Hello"
+    
+    return f"{msg} {name}"
+
+say_hello("AHmed")
+
