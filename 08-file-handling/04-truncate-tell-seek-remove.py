a2dfbacdf2637b4898a4c711666ac1b0498142cf
Eyad-Elghonemy <eyad0758@gmail.com>
2026-09-02 08:23:18 +0300

    Add 04-truncate-tell-seek-remove.py

---
+++ 08-file-handling/04-truncate-tell-seek-remove.py
@@ -0,0 +1,18 @@
+# -------------------------------------
+# -- File Handling => Important Info --
+# -------------------------------------
+
+myfile = open(r"C:\Users\eyad0\Documents\python\osama.txt", "a")
+myfile.truncate(5)
+
+# myfile = open(r"C:\Users\eyad0\Documents\python\osama.txt", "a")
+# print(myfile.tell())
+
+# myfile = open(r"C:\Users\eyad0\Documents\python\osama.txt", "r")
+
+# myfile.seek(11)
+# print(myfile.read())
+
+# import os
+
+# os.remove(r"C:\Users\eyad0\Documents\python\osama.txt")
