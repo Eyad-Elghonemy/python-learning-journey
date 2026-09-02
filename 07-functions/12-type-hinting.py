6cc038e52c0e692037265115096c7c647680e8c2
Eyad-Elghonemy <eyad0758@gmail.com>
2026-09-02 08:21:51 +0300

    Add 12-type-hinting.py

---
+++ 07-functions/12-type-hinting.py
@@ -0,0 +1,19 @@
+# ------------------
+# -- Type Hinting --
+# ------------------
+
+# def say_hello(name) -> str :
+    
+#     print(f"Hello {name}")
+    
+    
+
+# say_hello("Ali")
+
+def calc(n1, n2) -> int :
+    
+    print(n1 + n2)
+    
+calc(10, 5)
+        
+        
