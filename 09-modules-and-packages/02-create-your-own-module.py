42c75244e4ed7f16bf36a5c34327cd0d2b3f0f14
Eyad-Elghonemy <eyad0758@gmail.com>
2026-09-03 12:55:32 +0300

    Add 02-create-your-own-module.py

---
+++ 09-modules-and-packages/02-create-your-own-module.py
@@ -0,0 +1,42 @@
+# -----------------------------------
+# -- Modules => Create Your Module --
+# -----------------------------------
+
+# import sys
+# sys.path.append(r"D:\Games")
+# print(sys.path)
+
+# import  elzero
+
+# print(dir(elzero))
+
+# elzero.say_hello("Ahmed")
+# elzero.say_hello("Mohamed")
+# elzero.say_hello("Ali")
+
+# elzero.say_howareyou("Ahmed")
+# elzero.say_howareyou("Mohamed")
+# elzero.say_howareyou("Ali")
+
+# Alias
+
+# import  elzero as ee
+
+# print(dir(ee))
+
+# ee.say_hello("Ahmed")
+# ee.say_hello("Mohamed")
+# ee.say_hello("Ali")
+
+# ee.say_howareyou("Ahmed")
+# ee.say_howareyou("Mohamed")
+# ee.say_howareyou("Ali")
+
+# from elzero import say_hello
+
+# say_hello("Ali")
+
+from elzero import say_hello as ss
+
+ss("Ali")
+
