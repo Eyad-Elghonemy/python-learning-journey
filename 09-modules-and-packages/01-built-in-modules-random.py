2355537ef7a8a953d7d2de5104601febe76b5f13
Eyad-Elghonemy <eyad0758@gmail.com>
2026-09-03 12:55:13 +0300

    Add /09-modules-and-packages

---
+++ 09-modules-and-packages/01-built-in-modules-random.py
@@ -0,0 +1,32 @@
+# --------------------------------
+# -- Module => Built In Modules --
+# --------------------------------
+# [1] Module Is A file Contain A Set Of Functions
+# [2] You Can Import Module In Your App To Help You
+# [3] You Can Import Muiltiple Modules
+# [4]  You Can Create Your Own Modules
+# [5] Modules Saves Your Time
+# --------------------------------
+
+# import main Module
+
+# import random
+
+# print(random)
+
+# print(f"Print Random Float Number {random.random()}")
+
+# Show All Functions Inside Module
+
+# import random
+
+# print(dir(random))
+
+# Import One Or Two Functions From Module
+
+from random import randint , random
+
+print(F"Print Random Float {random()}")
+print(F"Print Random Integer {randint(100, 900)}")
+
+# from random import *  [All Functions]
